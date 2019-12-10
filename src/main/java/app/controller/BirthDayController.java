b7bb13387fc0faa57648b4179eba0e476f235d44
Darshan Redkar <darshan.redkar@zinfinity.in>
2019-12-10 20:00:29 +0530

    minor birth date
    d r

--- src/main/java/app/controller/BirthDayController.java
+++ src/main/java/app/controller/BirthDayController.java
@@ -1,8 +1,8 @@
 package app.controller;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -12,61 +12,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import app.data.entity.Contact;
-import app.data.entity.Members;
-import app.data.entity.RegisteredUsers;
 import app.data.repository.regular.ContactRepoReg;
-import app.data.repository.regular.RTrustRepo;
-import app.data.repository.regular.RUserRepo;
+
 
 @CrossOrigin(allowedHeaders="*")
 @RestController
 @RequestMapping("dob")
 public class BirthDayController {
 
-	@Autowired private RUserRepo userrepo;
-	@Autowired private RTrustRepo trustrepo;
 	@Autowired private ContactRepoReg contactrepo;
-	
 	
 	@GetMapping("getForToday/{td}")
 	public List<Contact> getTodaysBOD(@PathVariable String td){
-		
-//		List<Contact> finalvalue = new ArrayList<>();
-//		
-//		List<RegisteredUsers> rusers = userrepo.findByDob(td);
-//		
-//		List<Members> members = new ArrayList<>();
-//		List<Members> members2 = new ArrayList<Members>();
-//		trustrepo.findAll().stream().forEach(value -> value.getMembers().forEach(data ->
-//		{
-//			if(data != null ) {
-//				members.add(data);
-//			}
-//		}
-//		));
-//		members2 = members.stream().filter(data -> data.getDob().equals(td)).collect(Collectors.toList());
-//		
-//		List<Contact> contacts = contactrepo.findByDob(td);
-//		
-//		rusers.stream().forEach(data ->{
-//			if(data != null) {
-//				Contact c  = Contact.builder().name(data.getName()).dob(data.getDob()).build();
-//				finalvalue.add(c);
-//			}
-//		});
-//		
-//		members2.stream().forEach(data ->{
-//			Contact c  = Contact.builder().name(data.getMemname()).dob(data.getDob()).build();
-//			finalvalue.add(c);
-//		});
-//		contacts.stream().forEach(data ->{
-//			Contact c  = Contact.builder().name(data.getName()).dob(data.getDob()).build();
-//			finalvalue.add(c);
-//		});
-//		
-//		return finalvalue;
-//	}
-		return  null;
+		System.out.println("beduk"+td);
+		List<Contact> finalC = new ArrayList<>();
+		contactrepo.findAll().stream().forEach(data ->{
+			System.out.println(data.getDob());
+			if(LocalDate.parse(td).equals(data.getDob())) {
+				finalC.add(data);
+			}
+		});
+		return finalC;
 	}
 	
 }
--- src/main/java/app/data/repository/regular/ContactRepoReg.java
+++ src/main/java/app/data/repository/regular/ContactRepoReg.java
@@ -1,12 +1,15 @@
 package app.data.repository.regular;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import app.data.entity.Contact;
 
 public interface ContactRepoReg extends MongoRepository<Contact, String>{
 
-	public List<Contact> findByDob(String dob);
+	@Query("{ 'dob' : ?0 }")
+	public List<Contact> findByDob(LocalDate dob);
 }
